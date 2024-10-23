b6dd6aebb7b5881b3dcc31396330d61b91e06dd5
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-10-23 23:36:41 +0300

    start practice

---
+++ Unit1_FirstAndroidApp/app/src/main/java/com/example/unit1firstandroidapp/Practice.kt
@@ -0,0 +1,27 @@
+package com.example.unit1firstandroidapp
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unit1firstandroidapp.ui.theme.Unit1FirstAndroidAppTheme
+
+
+@Composable
+fun Practice(){
+    Column() {
+
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PracticePreview() {
+    Unit1FirstAndroidAppTheme {
+        Practice()
+
+    }
+}
