bced79c28875a50c56b59e3e86e8064160426559
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-10-23 23:46:52 +0300

    completed practice 1

--- Unit1_FirstAndroidApp/app/src/main/java/com/example/unit1firstandroidapp/Practice.kt
+++ Unit1_FirstAndroidApp/app/src/main/java/com/example/unit1firstandroidapp/Practice.kt
@@ -1,15 +1,60 @@
 package com.example.unit1firstandroidapp
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.unit1firstandroidapp.ui.theme.Unit1FirstAndroidAppTheme
 
 
 @Composable
-fun Practice(){
-    Column() {
+fun Practice(modifier: Modifier = Modifier){
+    Column(modifier = modifier){
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+
+        Text(
+            text = stringResource(R.string.jetpack_compose_tutorial),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.compose_description),
+            fontSize = 16.sp,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.tuttorial_description),
+            fontSize = 16.sp,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+
+@Composable
+fun Practice2(modifier: Modifier){
+    Column(
+        modifier= Modifier
+    ) {
 
     }
 }
@@ -21,7 +66,9 @@ fun Practice(){
 @Composable
 fun PracticePreview() {
     Unit1FirstAndroidAppTheme {
-        Practice()
+        Practice2(
+            modifier = Modifier.fillMaxSize()
+        )
 
     }
 }
