5129abd58b241e4fc3e525682a3561a5441c149f
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-10-24 00:53:12 +0300

    completed practice 4

--- Unit1_FirstAndroidApp/app/src/main/java/com/example/unit1firstandroidapp/Practice.kt
+++ Unit1_FirstAndroidApp/app/src/main/java/com/example/unit1firstandroidapp/Practice.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -157,15 +160,62 @@ Practice 4
  */
 
 @Composable
-fun Practice4(modifier: Modifier){
+fun Practice4(modifier: Modifier = Modifier){
+    Column(
+        modifier= modifier.background(Color.Gray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.weight(1f)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.android_logo),
+                contentDescription = ""
+            )
+            Text(
+                text = "Full Name"
+            )
+            Text(text = "Title")
+        }
+
+        Column(
+        ) {
+            InfoCard(
+                icon = R.drawable.ic_phone,
+                info = "555555555"
+            )
+            InfoCard(
+                icon = R.drawable.ic_share,
+                info = "@AndroidDev"
+            )
+            InfoCard(
+                icon = R.drawable.ic_email,
+                info = "@AndroidDev"
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoCard(icon:Int,info:String){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(painter = painterResource(icon), contentDescription = "")
+        Text(info, modifier = Modifier.padding(10.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PracticePreview() {
     Unit1FirstAndroidAppTheme {
-        Practice3(
+        Practice4(
             modifier = Modifier.fillMaxSize()
         )
 
