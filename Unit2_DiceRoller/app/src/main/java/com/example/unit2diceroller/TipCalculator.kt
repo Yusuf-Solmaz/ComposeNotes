e2e533be4e6c892332a452770ac2ee54082b39be
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-10-26 13:57:33 +0300

    explanation state hoisting

---
+++ Unit2_DiceRoller/app/src/main/java/com/example/unit2diceroller/TipCalculator.kt
@@ -0,0 +1,10 @@
+package com.example.unit2diceroller
+
+/**
+State Hoisting Nedir?
+Jetpack Compose'ta state hoisting, bir bileşenin (composable) durumunu dışarıda tutarak, bileşenin
+içindeki durum yönetimini üst bileşenlere devretme yöntemidir. Bu yaklaşım, bileşenlerin daha tekrar
+kullanılabilir ve test edilebilir olmasını sağlar.
+
+ */
+
