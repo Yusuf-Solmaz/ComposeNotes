4bd6fb8b7c8824b4b2e8817af2db5f4887530353
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-08 22:44:29 +0300

    explanation activity lifecycle

--- Unit4_NavigationAndAppArchitecture/app/src/main/java/com/example/unit4_navigationandapparchitecture/MainActivity.kt
+++ Unit4_NavigationAndAppArchitecture/app/src/main/java/com/example/unit4_navigationandapparchitecture/MainActivity.kt
@@ -59,6 +59,62 @@ import com.example.unit4_navigationandapparchitecture.ui.theme.Unit4_NavigationA
 
 /**
  Bir Android uygulamasında birden fazla aktivite olabilir. Ancak, tek bir aktivite kullanılması önerilmektedir
+
+ -------------------------------
+
+ onCreate() metodu yalnızca bir kez, aktivite ilk kez oluşturulduğunda çağrılır. onStart() metodu ise,
+ aktivite her görünür hale geldiğinde çağrılabilir.
+
+ -------------------------------
+
+ onStart ve onResume'da activity görünür haldedir. Sadece onResume'da kullanıcı activity'i ile etkileşime
+ girebilir.
+
+ -------------------------------
+
+ onCreate() ve onDestroy(), bir aktivite örneğinin yaşam süresi boyunca yalnızca bir kez çağrılır:
+ onCreate() uygulamayı ilk kez başlatmak için ve onDestroy() aktivitenin kullanmakta olduğu nesneleri
+ boşaltmak, kapatmak veya yok etmek için çağrılır, böylece bu nesneler kaynakları (örneğin belleği)
+ kullanmaya devam etmez.
+
+ -------------------------------
+
+ Uygulamanız arka planda iken, sistem kaynaklarını ve pil ömrünü korumak için aktif olarak çalışmamalıdır.
+ Uygulamanız arka plana geçtiğinde devam eden işlemleri duraklatmak, ön plana geçtiğinde ise bu işlemleri
+ yeniden başlatmak için Aktivite yaşam döngüsü ve geri çağrılarını kullanırsınız.
+
+ -------------------------------
+
+ Uygulama arka plana alındığında onPause() (focus kaybı) ve onStop() (UI'ın ekranda görünmemesi) metodu çağrılır.
+ Aktivite durdurulmuş olsa da Aktivite nesnesi arka planda bellekte tutulur. Android işletim sistemi aktiviteyi kapatmamıştır.
+ Kullanıcı uygulamaya geri dönebileceği için Android aktivite kaynaklarını bellekte tutar.
+
+ -------------------------------
+
+ Uygulama tekrar açıldığında onRestart(), onStart() ve onResume() metodu çağrılır. onRestart() metodu,
+ aktivitenizin ilk kez başlatılmadığı durumlarda çalıştırmak istediğiniz kodları koyabileceğiniz bir yerdir.
+
+ Aktivite tekrar ön plana döndüğünde onCreate() metodu yeniden çağrılmaz. Aktivite nesnesi yok edilmediği
+ için tekrar oluşturulması gerekmez.
+
+ -------------------------------
+
+ Bir uygulama başlatıldığında ve onStart() çağrıldığında, uygulama ekranda görünür hale gelir. onResume()
+ çağrıldığında, uygulama kullanıcı odağını kazanır
+
+ Uygulama arka plana geçtiğinde, onPause()’dan sonra odağını kaybeder ve onStop()’tan sonra artık görünür
+ olmaz.
+
+ Odağı ve görünürlüğü ayırt etmek önemlidir. Bir aktivite ekranda kısmen görünür olabilir ancak kullanıcı
+ odağına sahip olmayabilir.
+
+ -------------------------------
+
+ onPause() içinde çalışan herhangi bir kod diğer şeylerin görüntülenmesini engeller, bu yüzden onPause()
+ içindeki kodu hafif tutmak önemlidir. Örneğin, bir telefon araması geldiğinde, onPause()'daki kod gelen
+ arama bildirimini geciktirebilir.
+
+ -------------------------------
  */
 
 private const val TAG = "MainActivity"
