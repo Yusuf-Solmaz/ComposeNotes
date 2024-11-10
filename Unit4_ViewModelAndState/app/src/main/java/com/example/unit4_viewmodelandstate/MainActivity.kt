65fb0bfda344c8a77a3867f4d25dd67d052529aa
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-10 14:41:14 +0300

    add Game Screen

--- Unit4_ViewModelAndState/app/src/main/java/com/example/unit4_viewmodelandstate/MainActivity.kt
+++ Unit4_ViewModelAndState/app/src/main/java/com/example/unit4_viewmodelandstate/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.unit4_viewmodelandstate.ui.GameScreen
 import com.example.unit4_viewmodelandstate.ui.theme.Unit4_ViewModelAndStateTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +22,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Unit4_ViewModelAndStateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    GameScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Unit4_ViewModelAndStateTheme {
-        Greeting("Android")
     }
 }
