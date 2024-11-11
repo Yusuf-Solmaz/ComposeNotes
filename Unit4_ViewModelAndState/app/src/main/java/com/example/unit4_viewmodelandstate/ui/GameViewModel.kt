5f0e3563c96b695363c9836ddb9faaa5194ea4eb
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-11 17:19:42 +0300

    add required states

--- Unit4_ViewModelAndState/app/src/main/java/com/example/unit4_viewmodelandstate/ui/GameViewModel.kt
+++ Unit4_ViewModelAndState/app/src/main/java/com/example/unit4_viewmodelandstate/ui/GameViewModel.kt
@@ -1,14 +1,28 @@
 package com.example.unit4_viewmodelandstate.ui
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.unit4_viewmodelandstate.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.unit4_viewmodelandstate.data.DEFAULT_REMAINING_COUNT
+import com.example.unit4_viewmodelandstate.data.MAX_NO_OF_WORDS
+import com.example.unit4_viewmodelandstate.data.SCORE_INCREASE
+import kotlinx.coroutines.flow.update
+
 
 data class GameUiState(
-    val currentScrambledWord: String = ""
-)
+    val currentScrambledWord: String = "",
+    val isGuessedWordWrong: Boolean = false,
+    val score: Int = 0,
+    val currentWordCount: Int = 1,
+    val isGameOver: Boolean = false,
+    val remainingCount: Int = DEFAULT_REMAINING_COUNT
+    )
 
 /**
  ViewModel, UI’nin tükettiği durumu tutar ve açığa çıkarır.
@@ -28,6 +42,8 @@ class GameViewModel : ViewModel() {
     private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf()
 
+    var userGuess by mutableStateOf("")
+        private set
 
     init {
         resetGame()
@@ -37,6 +53,7 @@ class GameViewModel : ViewModel() {
     private fun pickRandomWordAndShuffle(): String {
         // Tüm kelimeler listesinden rastgele bir kelime seçilir
         currentWord = allWords.random()
+        Log.d("GameFragment", "currentWord is $currentWord")
 
         // Eğer seçilen kelime daha önce kullanıldıysa, yeniden rastgele kelime seçmeye çalışır
         if (usedWords.contains(currentWord)) {
@@ -69,6 +86,60 @@ class GameViewModel : ViewModel() {
         return String(tempWord)
     }
 
+    fun updateUserGuess(guessedWord: String){
+        userGuess = guessedWord
+    }
+
+    fun checkUserGuess() {
+        if (userGuess.equals(currentWord, ignoreCase = true)) {
+            // User's guess is correct, increase the score
+            // and call updateGameState() to prepare the game for next round
+            val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
+            updateGameState(updatedScore)
+        } else {
+            // User's guess is wrong, show an error
+            _uiState.update { currentState ->
+                currentState.copy(isGuessedWordWrong = true, remainingCount = currentState.remainingCount.dec())
+            }
+            if (_uiState.value.remainingCount == 0){
+                _uiState.update { currentState ->
+                    currentState.copy(isGameOver = true)
+                }
+            }
+        }
+        // Reset user guess
+        updateUserGuess("")
+    }
+
+    private fun updateGameState(updatedScore: Int) {
+        if (usedWords.size == MAX_NO_OF_WORDS){
+            //Last round in the game, update isGameOver to true, don't pick a new word
+            Log.d("GameFragment", "remainingCount: ${_uiState.value.remainingCount},isGameOver: ${_uiState.value.isGameOver}")
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else{
+            // Normal round in the game
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    currentWordCount = currentState.currentWordCount.inc(),
+                    score = updatedScore
+                )
+            }
+        }
+    }
+
+    fun skipWord() {
+        updateGameState(_uiState.value.score)
+        // Reset user guess
+        updateUserGuess("")
+    }
 
     fun resetGame() {
         usedWords.clear()
