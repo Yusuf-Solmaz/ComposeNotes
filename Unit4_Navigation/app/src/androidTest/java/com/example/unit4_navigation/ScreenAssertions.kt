2462cde97759b374c6ecd6372a40af70ccad8acf
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-13 21:14:42 +0300

    add NavController extension method

---
+++ Unit4_Navigation/app/src/androidTest/java/com/example/unit4_navigation/ScreenAssertions.kt
@@ -0,0 +1,8 @@
+package com.example.unit4_navigation
+
+import androidx.navigation.NavController
+import org.junit.Assert.assertEquals
+
+fun NavController.assertCurrentRouteName(expectedRouteName: String) {
+    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
