6b529546d66647a337af4d26fea0a1db0df33e72
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-13 15:13:52 +0300

    update destination

--- Unit4_Navigation/app/src/main/java/com/example/unit4_navigation/ui/presentation/navigation/CupcakeDestination.kt
+++ Unit4_Navigation/app/src/main/java/com/example/unit4_navigation/ui/presentation/navigation/CupcakeDestination.kt
@@ -4,9 +4,9 @@ import androidx.annotation.StringRes
 import com.example.unit4_navigation.R
 
 // Enum'daki name parametresi Enumun adlandırmasını döndürür. Örneğin CupcakeDestination.START.name -> START
-enum class CupcakeDestination(@StringRes val route: Int, @StringRes val  title: Int) {
-    START(route = R.string.app_name, title = R.string.start_order_title),
-    FLAVOR(route = R.string.choose_flavor, title = R.string.choose_flavor),
-    PICKUP(route = R.string.choose_pickup_date, title = R.string.choose_pickup_date),
-    SUMMARY(route = R.string.order_summary, title = R.string.order_summary)
+enum class CupcakeDestination(@StringRes val  title: Int) {
+    START(title = R.string.start_order_title),
+    FLAVOR(title = R.string.choose_flavor),
+    PICKUP(title = R.string.choose_pickup_date),
+    SUMMARY(title = R.string.order_summary)
 }
