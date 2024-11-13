404aa863dea983fc49870ab75628d6fe501bc6ef
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-13 12:16:03 +0300

    explanation navigation

---
+++ Unit4_Navigation/app/src/main/java/com/example/unit4_navigation/ui/presentation/navigation/CupcakeNavGraph.kt
@@ -0,0 +1,118 @@
+package com.example.unit4_navigation.ui.presentation.navigation
+
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.unit4_navigation.data.DataSource
+import com.example.unit4_navigation.ui.presentation.order.StartOrderScreen
+import com.example.unit4_navigation.ui.presentation.order.state.OrderUiState
+import com.example.unit4_navigation.R
+import com.example.unit4_navigation.ui.presentation.option.SelectOptionScreen
+import com.example.unit4_navigation.ui.presentation.order.viewmodel.OrderViewModel
+import com.example.unit4_navigation.ui.presentation.summary.OrderSummaryScreen
+
+/**
+NavController: Uygulamanızdaki ekranlar arasında geçiş yapmaktan sorumludur.
+NavGraph: Geçiş yapılacak bileşenleri eşleştirir.
+NavHost: NavGraph'ın geçerli ekranını göstermek için kapsayıcı olarak işlev gören bir composable'dır.
+ */
+
+/* Bu kodda OrderViewModel üzerindeki metodlar ile sadece StartOrderScreen'de değil ayrıca diğer screen'lerde de
+   higher-order fonksiyonlar yardımıyla kullanılmıştır. Böylece OrderUiState'deki değişimleri diğer screen'lerde
+   observe edebiliriz.
+ */
+
+/**
+ Ekranlar arası geçişte best practice navController'ı screen'de argüman olarak vermek yerine, screende higher order ile
+ fonksiyonu alıp NavHost içinde navController işlemlerini yapmak daha doğru olacaktır.
+ */
+
+@Composable
+fun CupcakeNavigation(
+    navController: NavHostController,
+    cupcakeUiState: OrderUiState,
+    viewModel: OrderViewModel = viewModel(),
+    modifier: Modifier = Modifier,
+    title: (String) -> Unit
+) {
+    NavHost(
+        navController = navController,
+        startDestination = CupcakeDestination.START.name,
+        modifier = modifier.fillMaxSize()
+    ) {
+
+
+        composable(route = CupcakeDestination.START.name) {
+            StartOrderScreen(
+                quantityOptions = DataSource.quantityOptions,
+                onNextButtonClicked = {
+                    viewModel.setQuantity(it)
+                    navController.navigate(CupcakeDestination.FLAVOR.name)
+                },
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(dimensionResource(R.dimen.padding_medium))
+            )
+            title(stringResource(CupcakeDestination.START.title))
+        }
+        composable(route = CupcakeDestination.FLAVOR.name) {
+            val context = LocalContext.current
+            SelectOptionScreen(
+                subtotal = cupcakeUiState.price,
+                onNextButtonClicked = { navController.navigate(CupcakeDestination.PICKUP.name) },
+                onCancelButtonClicked = {
+                    cancelOrderAndNavigateToStart(viewModel, navController)
+                },
+                options = DataSource.flavors.map { id -> context.resources.getString(id) },
+                onSelectionChanged = { viewModel.setFlavor(it) },
+                modifier = Modifier.fillMaxHeight()
+            )
+            title(stringResource(CupcakeDestination.FLAVOR.title))
+        }
+        composable(route = CupcakeDestination.PICKUP.name) {
+            SelectOptionScreen(
+                subtotal = cupcakeUiState.price,
+                onNextButtonClicked = { navController.navigate(CupcakeDestination.SUMMARY.name) },
+                onCancelButtonClicked = {
+                    cancelOrderAndNavigateToStart(viewModel, navController)
+                },
+                options = cupcakeUiState.pickupOptions,
+                onSelectionChanged = { viewModel.setDate(it) },
+                modifier = Modifier.fillMaxHeight()
+            )
+            title(stringResource(CupcakeDestination.PICKUP.title))
+        }
+        composable(route = CupcakeDestination.SUMMARY.name) {
+            OrderSummaryScreen(
+                orderUiState = cupcakeUiState,
+                onCancelButtonClicked = {
+                    cancelOrderAndNavigateToStart(viewModel, navController)
+                },
+                onSendButtonClicked = { subject: String, summary: String ->
+
+                },
+                modifier = Modifier.fillMaxHeight()
+            )
+            title(stringResource(CupcakeDestination.SUMMARY.title))
+        }
+    }
+}
+
+private fun cancelOrderAndNavigateToStart(
+    viewModel: OrderViewModel,
+    navController: NavHostController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(CupcakeDestination.START.name, inclusive = false)
+}
+
