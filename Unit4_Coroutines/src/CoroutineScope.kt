2d0642a71fb2047f9624df4f13578f71dbf94851
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-25 15:09:09 +0300

    explanation CoroutineContext

--- Unit4_Coroutines/src/CoroutineScope.kt
+++ Unit4_Coroutines/src/CoroutineScope.kt
@@ -1,4 +1,5 @@
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 /*
  coroutineScope: bir kapsam (scope) oluşturur ve bu kapsamda başlatılan tüm coroutine'ler birbiriyle ilişkili olarak çalışır.
@@ -8,6 +9,41 @@ import kotlinx.coroutines.*
 
  */
 
+/*
+ Activity'de sağlanan coroutine scope olan lifecycleScope içinde bir coroutine başlatırsanız, eğer activity yok edilirse,
+ lifecycleScope iptal edilir ve içindeki tüm çocuk coroutin'ler de otomatik olarak iptal edilir. Activity'nin yaşam döngüsünü
+ takip eden bir coroutine'in bu davranışın istediğiniz bir şey olup olmadığına karar vermeniz gerekir. Bu durum viewModelScope
+ için de aynıdır.
+*/
+
+/*
+ launch() ve async() CoroutineScope interface'sinin extension fonksiyonlarıdır. CoroutineScope içinde CoroutineContext barındırır.
+ CoroutineContext, coroutine'in çalışacağı bağlam hakkında bilgi sağlar. CoroutineContext, her öğesinin benzersiz bir anahtarı olan
+ bir harita (map) gibidir. Bunlar zorunlu alanlar değildir, ancak bağlamda bulunabilecek bazı örnekler şunlardır:
+
+ name - coroutine'i benzersiz bir şekilde tanımlamak için kullanılan isim
+ job - coroutine'in yaşam döngüsünü kontrol eder
+ dispatcher - işi uygun thread'e yönlendirir
+ exception handler - coroutine içinde çalıştırılan kod tarafından fırlatılan istisnaları işler
+
+ Not: Bunlar, CoroutineContext'in varsayılan değerleridir ve eğer bunlar için değer sağlanmazsa kullanılacaklardır:
+
+ "coroutine" coroutine ismi olarak
+ parent job yok
+ Dispatchers.Default coroutine dispatcher'ı olarak
+ exception handler yok
+
+ Bir coroutine içinde yeni bir coroutine başlattığınızda, çocuk coroutine, ebeveyn coroutine'den CoroutineContext'i miras alır,
+ ancak sadece yeni oluşturulan coroutine için job'ı özel olarak değiştirir. Ayrıca, launch() veya async() fonksiyonlarına,
+ bağlamın farklı olmasını istediğiniz bölümleri geçirmek suretiyle, ebeveyn context'ten miras alınan öğeleri de geçersiz
+ kılabilirsiniz.
+
+ scope.launch(Dispatchers.Default) {
+    ...
+ }
+
+
+*/
 
 fun main() {
     runBlocking {
