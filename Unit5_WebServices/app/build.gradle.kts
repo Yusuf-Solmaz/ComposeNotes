0b3cd974d7cfd6404e091f277dda741fc5e6248b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-11-29 23:35:45 +0300

    add coroutine test dependencies

--- Unit5_WebServices/app/build.gradle.kts
+++ Unit5_WebServices/app/build.gradle.kts
@@ -84,4 +84,14 @@ dependencies {
     // Retrofit with Scalar Converter
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     //json to string
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
 }
