72fa05c80e105ec4b48d82adbf793172d96126ce
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-01 14:33:20 +0300

    upadte package

--- Unit4_AdaptiveApp/basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/utils/WindowStateUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.reply.utils
-
-enum class ReplyNavigationType {
-    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
-}
-
-enum class ReplyContentType {
-    LIST_ONLY, LIST_AND_DETAIL
-}
