f38b84fbcf7d26a86ffeb2568d98afcb90785c86
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-01 14:32:12 +0300

    add mars photo grid

--- Unit5_WebServices/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ Unit5_WebServices/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -17,22 +17,33 @@ package com.example.marsphotos.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.marsphotos.R
+import com.example.marsphotos.data.network.mars_photos.model.MarsPhoto
 import com.example.marsphotos.ui.screens.state.MarsUIState
 import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
@@ -41,6 +52,7 @@ fun HomeScreen(
     marsUiState: MarsUIState,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
+    retryAction :() -> Unit
 ) {
     when(marsUiState){
         is MarsUIState.Loading -> {
@@ -48,11 +60,11 @@ fun HomeScreen(
         }
         is MarsUIState.Success -> {
             ResultScreen(
-                marsUiState.photos, modifier = modifier.fillMaxWidth()
+                marsUiState.photos, modifier = modifier.padding(contentPadding)
             )
         }
         is MarsUIState.Error -> {
-            ErrorScreen( errorMessage = marsUiState.error, modifier = modifier.fillMaxSize())
+            ErrorScreen( retryAction = retryAction,errorMessage = marsUiState.error, modifier = modifier.fillMaxSize())
         }
     }
 }
@@ -61,22 +73,63 @@ fun HomeScreen(
  * ResultScreen displaying number of photos retrieved.
  */
 @Composable
-fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-    ) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier
-        ) {
-            Text(text = photos)
+fun ResultScreen(photos: List<MarsPhoto>, modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier.fillMaxSize()) {
+        MarsPhotoGrid(photos)
+    }
+}
+
+@Composable
+fun MarsPhotoGrid(
+    photos: List<MarsPhoto>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)){
+
+    /**
+    Bu kodda kullanılan key parametresi, Compose'un öğeleri yeniden sıralarken veya yeniden çizim
+    yaparken doğru durumu (state) korumasına yardımcı olur. Özellikle, LazyVerticalGrid gibi listelerde
+    öğeler dinamik olarak eklendiğinde, silindiğinde veya sıralandığında, Compose hangi öğenin nerede
+    olduğunu doğru bir şekilde takip etmek için key değerini kullanır.
+     */
+
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.padding(horizontal = 4.dp),
+        contentPadding = contentPadding) {
+        items(items = photos, key = {photo -> photo.id}) {
+                photo ->
+
+            MarsImage(photo,modifier = modifier
+                .padding(4.dp)
+                .fillMaxWidth()
+                .aspectRatio(1.5f))
         }
     }
 }
 
 @Composable
-fun ErrorScreen(errorMessage: String,modifier: Modifier = Modifier) {
+fun MarsImage(photoUrl: MarsPhoto,modifier: Modifier= Modifier) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(photoUrl.image)
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(R.drawable.ic_launcher_background),
+            error = painterResource(R.drawable.ic_connection_error),
+            contentDescription = stringResource(R.string.mars_photo),
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
+fun ErrorScreen(retryAction :()-> Unit, errorMessage: String, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
@@ -86,6 +139,9 @@ fun ErrorScreen(errorMessage: String,modifier: Modifier = Modifier) {
             painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
         )
         Text(text = errorMessage, modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
     }
 }
 
@@ -104,6 +160,7 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
 @Composable
 fun ResultScreenPreview() {
     MarsPhotosTheme {
-
+        val mockData = List(10) { MarsPhoto("$it", "") }
+        MarsPhotoGrid(mockData)
     }
 }
