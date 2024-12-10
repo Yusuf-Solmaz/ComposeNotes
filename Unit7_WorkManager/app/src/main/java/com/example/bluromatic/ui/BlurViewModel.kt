76d87a2d470d072f2682eea2e9570f1be119f5d2
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-12-10 16:12:11 +0300

    add stateIn func

--- Unit7_WorkManager/app/src/main/java/com/example/bluromatic/ui/BlurViewModel.kt
+++ Unit7_WorkManager/app/src/main/java/com/example/bluromatic/ui/BlurViewModel.kt
@@ -19,23 +19,46 @@ package com.example.bluromatic.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.work.WorkInfo
 import com.example.bluromatic.BluromaticApplication
+import com.example.bluromatic.STOP_TIMEOUT_MILLIS
 import com.example.bluromatic.data.BlurAmountData
 import com.example.bluromatic.data.BluromaticRepository
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 /**
  * [BlurViewModel] starts and stops the WorkManger and applies blur to the image. Also updates the
  * visibility states of the buttons depending on the states of the WorkManger.
  */
+
 class BlurViewModel(private val bluromaticRepository: BluromaticRepository) : ViewModel() {
 
     internal val blurAmount = BlurAmountData.blurAmount
 
-    val blurUiState: StateFlow<BlurUiState> = MutableStateFlow(BlurUiState.Default)
+    val blurUiState: StateFlow<BlurUiState> = bluromaticRepository.outputWorkInfo
+        .map {
+        info ->
+        when{
+            info.state.isFinished ->{
+                BlurUiState.Complete(outputUri = "")
+            }
+            info.state == WorkInfo.State.CANCELLED -> {
+                BlurUiState.Default
+            }
+            else -> BlurUiState.Loading
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(STOP_TIMEOUT_MILLIS),
+        initialValue = BlurUiState.Default
+    )
+
 
     /**
      * Call the method from repository to create the WorkRequest to apply the blur
